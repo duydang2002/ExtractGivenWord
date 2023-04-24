2bc9c17a870e16d0666b4df7024ca9765a012828
Email <76654292+duydang2002@users.noreply.github.com>
2023-04-24 23:11:00 +0700

    Add project files.

---
+++ ExtractGivenWord/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Text;
+
+namespace extractGivenWord
+{
+    internal class ExtractGivenWord
+    {
+        public String extract(String text) {
+            StringBuilder res = new StringBuilder();
+            String[] senteces = text.Split('.');
+            List<String> inSentence = new List<String>();
+            foreach (var sentence in senteces)
+            {
+                String[] sen = sentence.Split(" ");
+                if (sen.Contains("in"))
+                {  
+                    inSentence.Add(sentence);
+                }
+            }
+            if (inSentence.Count > 0)
+            {
+                foreach(var sentence in inSentence)
+                {   String temp= sentence;
+                    if (sentence.ElementAt(0) == ' ') {temp =sentence.Remove(0, 1); 
+                    }
+                    res.Append(temp+"\n");
+                }
+                return res.ToString();
+            }
+            else return "";
+        }
+        static void Main(string[] args)
+        {
+            String text = "We are living in a yellow submarine. " +
+                "We don't have anything else. " +
+                "Inside the submarine is very tight." +
+                " So we are drinking all the day." +
+                " We will move out of it in 5 days.";
+            ExtractGivenWord e = new ExtractGivenWord();
+            Console.WriteLine(e.extract(text));
+        }
+        
+    }
+}
